95c8a60bbc636e2dbc340d70f36cc213002a1e49
jasminetam <jasminetam94@gmail.com>
2022-06-12 00:46:28 +0100

    fixing deployment error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import "./app.css";
 import BudgetCard from "../src/Components/BudgetCard";
 import BudgetInputModal from "../src/Components/BudgetInputModal";
